16a287324befe9726a9f6bf0df54cd8bdf2f8a76
abinmathew1996 <abin.m.n007@gmail.com>
2022-07-27 14:54:36 +0530

    validation1.1

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,42 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-// uname = "" 
-// acno = ""
-// pswd = ""
+  // uname = ""
+  // acno = ""
+  // pswd = ""
 
-//register model
-registerForm = this.fb.group({
-uname : [ "" ],
-acno : [""],
-pswd : [""]
-})
-  constructor(private ds:DataService, private router : Router, private fb:FormBuilder) { }
+  //register model
+  registerForm = this.fb.group({
+    uname: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+    acno: [''],
+    pswd: [''],
+  });
+  constructor(
+    private ds: DataService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
-  }
-  register(){
-    var uname = this.registerForm.value.uname
-    var acno = this.registerForm.value.acno
-    var pswd = this.registerForm.value.pswd
-
-    const result = this.ds.register(acno,uname,pswd)
-    if(result){
-      alert('sucessfully regestered!!!')
-this.router.navigateByUrl('')
-    }
-    else{
-      alert('user already exist')
-      this.router.navigateByUrl('')
-
+  ngOnInit(): void {}
+  register() {
+    var uname = this.registerForm.value.uname;
+    var acno = this.registerForm.value.acno;
+    var pswd = this.registerForm.value.pswd;
+    if (this.registerForm.valid) {
+      const result = this.ds.register(acno, uname, pswd);
+      if (result) {
+        alert('sucessfully regestered!!!');
+        this.router.navigateByUrl('');
+      } else {
+        alert('user already exist');
+        this.router.navigateByUrl('');
+      }
+    } else {
+      alert('Invalid form');
     }
   }
 }
